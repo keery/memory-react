a64e1ffb4629d6f2792a27ddf1e6c00b569588dc
keery <mon-compte1131@live.fr>
2018-04-26 20:59:20 +0200

    Initiation aux event et au boucle à l'aide de map

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 import './Card.css'
 
-const HIDDEN_SYMBOL = '❓'
+const HIDDEN_SYMBOL = '❓';
 
-const Card = ({ card, feedback }) => (
-    <div className={`card ${feedback}`}>
+
+const Card = ({ card, feedback, onClick }) => (
+    <div className={`card ${feedback}`}  onClick={() => {onClick(card)}}>
       <span className="symbol">
         {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
       </span>
