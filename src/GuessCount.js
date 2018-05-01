00b2d0f23845d4f2dfd5d33299574de812bb52ad
keery <mon-compte1131@live.fr>
2018-05-01 23:28:13 +0200

    Utilisation de state/setState

--- src/GuessCount.js
+++ src/GuessCount.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 import './GuessCount.css'
 
 const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>
 
+GuessCount.defaultProps = {
+    guesses: 0
+}
+
+GuessCount.propTypes = {
+    guesses: PropTypes.number.isRequired,
+}
 export default GuessCount
